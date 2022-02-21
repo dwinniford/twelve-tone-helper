674d3915ae01ab0b17241218756a5a7deb743968
David Winniford <dwinniford@gmail.com>
2022-02-21 15:51:30 -0600

    accepts user input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program 
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to 12 Tone Helper.  Would you like to generate a tone row? (y/n)");
+        String input = Console.ReadLine();
+        if(input == "y")
+        {
+            Console.WriteLine("Generating tone row...");
+        } else if(input == "n") 
+        {
+            Console.WriteLine("goodbye!");
+        }
+    }
+}
